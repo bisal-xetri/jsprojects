07c1653d8503ccd96d40ca732e378f99bfb0ac43
bisal-xetri <bishaldkl56@gmail.com>
2024-08-10 17:09:40 +0545

    working on scrolling

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -19,8 +21,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(btn=>btn.addEventListener('click', openModal)
-);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
@@ -34,6 +35,52 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+//Button Scrolling
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (x/Y)', window.pageXOffset, pageYOffset);
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  //scrolling
+  // window.scrollTo(s1coords.left,s1coords.right);
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//page Navigation
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id=this.getAttribute('href');
+//     // console.log(id);
+//     document.querySelector(id).scrollIntoView({behavior:'smooth'});
+//   });
+// });
+
+//1. add event Listener to a common parent element
+//2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  // console.log(e.target);
+
+  //matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    // console.log("LINK")
+    e.preventDefault();
+    const id = e.target.getAttribute('href');
+    // console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
 //select create and delete elements
 // console.log(document.documentElement);
 // console.log(document.head)
@@ -41,7 +88,6 @@ document.addEventListener('keydown', function (e) {
 //  const header=document.querySelector('.header');
 // // const allSection=document.querySelectorAll('.section');
 // // console.log(allSection)
-
 
 // // document.getElementById('section--1');
 // // const buttons=document.getElementsByTagName('button');
@@ -77,7 +123,6 @@ document.addEventListener('keydown', function (e) {
 
 // // message.style.height=Number.parseFloat(getComputedStyle(message).height,10)+40+'px';
 
-
 // document.documentElement.style.setProperty('--color-primary','red')
 // //attributes
 // const logo=document.querySelector('.nav__logo');
@@ -102,65 +147,41 @@ document.addEventListener('keydown', function (e) {
 //   logo.classList.toggle('c');
 //   logo.classList.contains('c');
 
-  //logo.className='bishal'//override all existing classes
+//logo.className='bishal'//override all existing classes
 
-  const btnScrollTo=document.querySelector('.btn--scroll-to');
-  const section1=document.querySelector('#section--1');
+// const h1=document.querySelector('h1');
+// const alertH1=function(e){
+//   alert('addEventListener')
 
+//   // h1.removeEventListener('mouseenter',alertH1);
+//   }
+//     // h1.addEventListener('mouseenter',alertH1);
 
-  btnScrollTo.addEventListener('click',function(e){
-    const s1coords=section1.getBoundingClientRect();
-    console.log(s1coords)
+//     // setTimeout(()=>h1.removeEventListener('mouseenter',alertH1),3000);
 
-  console.log(  e.target.getBoundingClientRect());
+//   // h1.onmouseenter=function(e){
+//   //   alert('hey')
+//   // }
 
+// //rgb(255,255,255)
+// const randomInt=(min,max)=>Math.floor(Math.random()*(max-min+1)+min);
 
-  console.log('Current scroll (x/Y)',window.pageXOffset,pageYOffset);
-  console.log('height/width viewport',document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
+// const randomColor=()=>`rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`
+// console.log(randomColor(0,255))
 
-  //scrolling
-  // window.scrollTo(s1coords.left,s1coords.right); 
+// document.querySelector('.nav__link').addEventListener('click',function(e){
+//   this.style.backgroundColor=randomColor();
+//   console.log('LINK',e.target,e.currentTarget)//e.currentTarget===this
 
-  section1.scrollIntoView({behavior:'smooth'})
-  })
-  
-const h1=document.querySelector('h1');
-const alertH1=function(e){
-  alert('addEventListener')
+//   //Stop propagation
 
-  // h1.removeEventListener('mouseenter',alertH1);
-  }
-    // h1.addEventListener('mouseenter',alertH1);
+// })
+// document.querySelector('.nav__links').addEventListener('click',function(e){
+//   this.style.backgroundColor=randomColor();
+//   console.log('Container',e.target,e.currentTarget)
+// })
 
-    // setTimeout(()=>h1.removeEventListener('mouseenter',alertH1),3000);
-
-  // h1.onmouseenter=function(e){
-  //   alert('hey')
-  // }
-
-
-//rgb(255,255,255)
-const randomInt=(min,max)=>Math.floor(Math.random()*(max-min+1)+min);
-
-const randomColor=()=>`rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`
-console.log(randomColor(0,255))
-
-
-document.querySelector('.nav__link').addEventListener('click',function(e){
-  this.style.backgroundColor=randomColor();
-  console.log('LINK',e.target,e.currentTarget)//e.currentTarget===this
-
-  //Stop propagation
-  
-})
-document.querySelector('.nav__links').addEventListener('click',function(e){
-  this.style.backgroundColor=randomColor();
-  console.log('Container',e.target,e.currentTarget)
-})
-
-document.querySelector('.nav').addEventListener('click',function(e){
-  this.style.backgroundColor=randomColor();
-  console.log('Nav',e.target,e.currentTarget)
-})
+// document.querySelector('.nav').addEventListener('click',function(e){
+//   this.style.backgroundColor=randomColor();
+//   console.log('Nav',e.target,e.currentTarget)
+// } )
